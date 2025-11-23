be512dfe722944321d1d62f08d6b26c7b19f111f
LegoWolf <glynwolf@gmail.com>
2025-11-22 17:20:17 -0700

    Updated layout to match my mini42 custom.
    - Got brutal about disabling unused features to stay in AVR memory.
    - No game layer.
    - Added logo scrolling on right hand OLED.

--- keyboards/crkbd/keymaps/davidf/keymap.c
+++ keyboards/crkbd/keymaps/davidf/keymap.c
@@ -21,9 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 typedef enum {
     LAYER_BASE,
-    LAYER_NAV,
-    LAYER_NUM,
-    LAYER_FUNC,
+    LAYER_NAVIGATION,
+    LAYER_NUMBER,
+    LAYER_FUNCTION,
     LAYER_SUPER
 } custom_layers_t;
 
@@ -38,46 +38,70 @@ typedef enum {
 #else
     KC_OSMODE = SAFE_RANGE,
 #endif
+    KC_A_CIRCUMFLEX,        // QK_USER_1
+    KC_A_DIAERESIS,         // QK_USER_2
+    KC_E_CIRCUMFLEX,        // QK_USER_3
+    KC_E_DIAERESIS,         // QK_USER_4
+    KC_I_CIRCUMFLEX,        // QK_USER_5
+    KC_I_DIAERESIS,         // QK_USER_6
+    KC_O_CIRCUMFLEX,        // QK_USER_7
+    KC_O_DIAERESIS,         // QK_USER_8
+    KC_U_CIRCUMFLEX,        // QK_USER_9
+    KC_U_DIAERESIS,         // QK_USER_10
 } custom_keycodes_t;
 
+#define KC_ACIR KC_A_CIRCUMFLEX
+#define KC_ADIA KC_A_DIAERESIS
+#define KC_ECIR KC_E_CIRCUMFLEX
+#define KC_EDIA KC_E_DIAERESIS
+#define KC_ICIR KC_I_CIRCUMFLEX
+#define KC_IDIA KC_I_DIAERESIS
+#define KC_OCIR KC_O_CIRCUMFLEX
+#define KC_ODIA KC_O_DIAERESIS
+#define KC_UCIR KC_U_CIRCUMFLEX
+#define KC_UDIA KC_U_DIAERESIS
 #define MAX_LAYERS (LAYER_SUPER + 1)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_split_3x6_3(
       KC_ESC,          KC_Q,        KC_W,          KC_E,           KC_R,          KC_T,                KC_Y,             KC_U,          KC_I,          KC_O,          KC_P,          KC_MINS,
       MO(LAYER_SUPER), KC_A,        KC_S,          KC_D,           KC_F,          KC_G,                KC_H,             KC_J,          KC_K,          KC_L,          KC_SCLN,       MO(LAYER_SUPER),
-      OSM(MOD_LSFT),   KC_Z,        KC_X,          KC_C,           KC_V,          KC_B,                KC_N,             KC_M,          KC_COMM,       KC_DOT,        CA_EACU,       OSM(MOD_RSFT),
+      KC_LSFT,         KC_Z,        KC_X,          KC_C,           KC_V,          KC_B,                KC_N,             KC_M,          KC_COMM,       KC_DOT,        CA_EACU,       KC_RSFT,
                                                    TL_LOWR,        KC_BSPC,       LCTL_T(KC_TAB),      KC_ENTER,         KC_SPACE,      TL_UPPR
   ),
 
-  [LAYER_NAV] = LAYOUT_split_3x6_3(
-      XXXXXXX,         XXXXXXX,     XXXXXXX,       XXXXXXX,        XXXXXXX,       XXXXXXX,             KC_PGUP,          KC_HOME,       KC_UP,         KC_END,        XXXXXXX,       XXXXXXX,
-      XXXXXXX,         KC_MPRV,     KC_MNXT,       KC_VOLD,        KC_VOLU,       KC_MPLY,             KC_PGDN,          KC_LEFT,       KC_DOWN,       KC_RIGHT,      XXXXXXX,       XXXXXXX,
-      _______,         XXXXXXX,     KC_LGUI,       KC_LCTL,        KC_LALT,       KC_MUTE,             XXXXXXX,          XXXXXXX,       MS_WHLU,       MS_WHLD,       XXXXXXX ,      _______,
+  [LAYER_NAVIGATION] = LAYOUT_split_3x6_3(
+      XXXXXXX,         XXXXXXX,     XXXXXXX,       KC_INS,         KC_DEL,        KC_MUTE,             KC_PGUP,          KC_HOME,       KC_UP,         KC_END,        MS_WHLU,       XXXXXXX,
+      XXXXXXX,         KC_MPRV,     KC_MNXT,       KC_VOLD,        KC_VOLU,       KC_MPLY,             KC_PGDN,          KC_LEFT,       KC_DOWN,       KC_RIGHT,      MS_WHLD,       XXXXXXX,
+      _______,         KC_APP,      KC_LGUI,       KC_LCTL,        KC_LALT,       KC_RALT,             OSM(MOD_RALT),    OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LGUI), XXXXXXX,       _______,
                                                    _______,        _______,       _______,             _______,          _______,       _______
   ),
 
-  [LAYER_NUM] = LAYOUT_split_3x6_3(
+  [LAYER_NUMBER] = LAYOUT_split_3x6_3(
       XXXXXXX,         S(KC_1),     S(KC_2),       S(KC_3),        S(KC_4),       S(KC_5),             S(KC_6),          S(KC_7),       S(KC_8),       S(KC_9),       S(KC_0),       XXXXXXX,
       XXXXXXX,         KC_1,        KC_2,          KC_3,           KC_4,          KC_5,                KC_6,             KC_7,          KC_8,          KC_9,          KC_0,          XXXXXXX,
       _______,         XXXXXXX,     XXXXXXX,       XXXXXXX,        XXXXXXX,       XXXXXXX,             XXXXXXX,          XXXXXXX,       XXXXXXX,       XXXXXXX,       XXXXXXX,       _______,
                                                    _______,        _______,       _______,             _______,          _______,       _______
   ),
 
-  [LAYER_FUNC] = LAYOUT_split_3x6_3(
-      XXXXXXX,         KC_F11,      KC_F12,        KC_F13,         KC_F14,        KC_F15,              KC_F16,           KC_BRID,       KC_BRIU,       KC_MCTL,       KC_LPAD,       XXXXXXX,
-      XXXXXXX,         KC_F1,       KC_F2,         KC_F3,          KC_F4,         KC_F5,               KC_F6,            KC_F7,         KC_F8,         KC_F9,         KC_F10,        XXXXXXX,
-      _______,         KC_PAUS,     KC_PSCR,       KC_NUM,         KC_SCRL,       XXXXXXX,             XXXXXXX,          XXXXXXX,       XXXXXXX,       XXXXXXX,       XXXXXXX,       _______,
+  [LAYER_FUNCTION] = LAYOUT_split_3x6_3(
+      KC_SLEP,         KC_F11,      KC_F12,        KC_F13,         KC_F14,        KC_F15,              KC_F16,           KC_BRID,       KC_BRIU,       KC_MCTL,       KC_LPAD,       XXXXXXX,
+      KC_OSMODE,       KC_F1,       KC_F2,         KC_F3,          KC_F4,         KC_F5,               KC_F6,            KC_F7,         KC_F8,         KC_F9,         KC_F10,        XXXXXXX,
+      _______,         KC_PAUS,     KC_NUM,        KC_SCRL,        KC_PSCR,       UG_TOGG,             UG_NEXT,          UG_HUEU,       UG_SATU,       UG_VALU,       UG_SPDU,       _______,
                                                    _______,        _______,       _______,             _______,          _______,       _______
   ),
 
   [LAYER_SUPER] = LAYOUT_split_3x6_3(
-      XXXXXXX,         XXXXXXX,     XXXXXXX,       CA_EGRV,        CA_DTIL,       KC_INS,              KC_DEL,           CA_UGRV,       CA_GRV,        CA_CIRC,       CA_DIAE,       KC_EQL,
-      XXXXXXX,         CA_AGRV,     CA_PIPE,       CA_LABK,        CA_RABK,       CA_BSLS,             CA_SLSH,          CA_LCBR,       CA_RCBR,       CA_LBRC,       CA_RBRC,       CA_PLUS,
-      KC_CAPS,         CA_LDAQ,     CA_RDAQ,       CA_CCED,        XXXXXXX,       XXXXXXX,             KC_APP,           OSM(MOD_LGUI), OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_RALT), KC_CAPS,
-                                                   _______,        _______,       _______,             _______,          _______,       KC_OSMODE
+      XXXXXXX,         CA_LABK,     CA_RABK,       CA_EGRV,        CA_DTIL,       S(CA_AGRV),          S(CA_CCED),       CA_UGRV,       CA_GRV,        CA_CIRC,       CA_DIAE,       KC_EQL,
+      KC_ADIA,         CA_AGRV,     KC_EDIA,       KC_IDIA,        KC_ODIA,       KC_UDIA,             KC_ACIR,          KC_ECIR,       KC_ICIR,       KC_OCIR,       KC_UCIR,       CA_PLUS,
+      _______,         CA_LDAQ,     CA_RDAQ,       CA_CCED,        CA_PIPE,       CA_BSLS,             CA_SLSH,          CA_LCBR,       CA_RCBR,       CA_LBRC,       CA_RBRC,       _______,
+                                                   _______,        _______,       _______,             _______,          _______,       _______
   )
 };
+
+// Key combinations.
+const uint16_t PROGMEM combo_shift_caps[] = { KC_LSFT, KC_RSFT, COMBO_END };
+combo_t key_combos[] = { COMBO(combo_shift_caps, KC_CAPS) };
 
 static os_modes_t os_mode = WIN;
 
@@ -423,6 +447,7 @@ bool oled_task_user(void) {
         }
     } else {
         oled_render_logo();
+        oled_scroll_left();
     }
     return false;
 }
@@ -451,6 +476,17 @@ static bool swap_key_event(uint16_t keycode, keyrecord_t *record)
     return true;
 }
 
+static bool send_deadkey_event(uint16_t deadkeycode, uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        uint8_t mods = get_mods() & (MOD_MASK_SHIFT | MOD_BIT(KC_RALT));
+        del_mods(mods);
+        tap_code16(deadkeycode);
+        add_mods(mods);
+        tap_code16(keycode);
+    }
+    return false;
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case KC_OSMODE:
@@ -459,6 +495,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 os_mode = (os_mode == WIN) ? MAC : WIN;
             }
             return false;
+
+        // Single-stroke versions of keys that normally require deadkey composition.
+        case KC_A_CIRCUMFLEX: return send_deadkey_event(CA_CIRC, KC_A, record);
+        case KC_A_DIAERESIS: return send_deadkey_event(CA_DIAE, KC_A, record);
+        case KC_E_CIRCUMFLEX: return send_deadkey_event(CA_CIRC, KC_E, record);
+        case KC_E_DIAERESIS: return send_deadkey_event(CA_DIAE, KC_E, record);
+        case KC_I_CIRCUMFLEX: return send_deadkey_event(CA_CIRC, KC_I, record);
+        case KC_I_DIAERESIS: return send_deadkey_event(CA_DIAE, KC_I, record);
+        case KC_O_CIRCUMFLEX: return send_deadkey_event(CA_CIRC, KC_O, record);
+        case KC_O_DIAERESIS: return send_deadkey_event(CA_DIAE, KC_O, record);
+        case KC_U_CIRCUMFLEX: return send_deadkey_event(CA_CIRC, KC_U, record);
+        case KC_U_DIAERESIS: return send_deadkey_event(CA_DIAE, KC_U, record);
 
         // Mac and Windows swap the placement of KC_NUBS and CA_SLSH under CSA keyboard layout.
         case KC_NONUS_BACKSLASH: return swap_key_event(CA_SLSH, record);
@@ -470,7 +518,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // Suppress keys that confuse the selected OS.
         case KC_PAUSE:
         case KC_SCROLL_LOCK:
-            return (os_mode == WIN);
+            return (os_mode != MAC);
         case KC_MISSION_CONTROL:
         case KC_LAUNCHPAD:
             return (os_mode == MAC);
